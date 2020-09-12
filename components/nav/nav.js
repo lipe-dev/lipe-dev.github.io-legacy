1576707818d29f7562dfb4103d79b5ce5c3840b3
Felipe Pereira <fe@lipe.dev>
2020-09-12 00:12:23 -0300

    add bottom versions of nav and navitem

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,16 +1,40 @@
-import React from "react";
+import cn from "classnames";
 
 import NavItem from "./navItem";
 
-export default function Nav({ home }) {
+export default function Nav({ home, bottom }) {
   return (
-    <nav className="container mx-auto flex items-center justify-center pb-3">
-      <NavItem href="/">Home</NavItem>
-      <NavItem href="/">Tech</NavItem>
-      <NavItem href="/">Folio</NavItem>
-      <NavItem href="/">Blog</NavItem>
-      <NavItem href="/">About</NavItem>
-      <NavItem href="/">Chat</NavItem>
+    <nav
+      className={cn(
+        "container",
+        "mx-auto",
+        "flex",
+        bottom ? "items-start" : "items-center",
+        "justify-center",
+        "pb-3",
+        bottom ? "text-gray-200" : "text-gray-700",
+        { "mt-8": bottom },
+        { "flex-col": bottom }
+      )}
+    >
+      <NavItem href="/" bottom={bottom}>
+        Home
+      </NavItem>
+      <NavItem href="/" bottom={bottom}>
+        Tech
+      </NavItem>
+      <NavItem href="/" bottom={bottom}>
+        Folio
+      </NavItem>
+      <NavItem href="/" bottom={bottom}>
+        Blog
+      </NavItem>
+      <NavItem href="/" bottom={bottom}>
+        About
+      </NavItem>
+      <NavItem href="/" bottom={bottom}>
+        Chat
+      </NavItem>
     </nav>
   );
 }
--- components/nav/navItem.js
+++ components/nav/navItem.js
@@ -1,9 +1,26 @@
 import Link from "next/link";
+import cn from "classnames";
 
-export default function NavItem({ href, children }) {
+export default function NavItem({ href, children, bottom }) {
   return (
     <Link href={href}>
-      <a className="px-3 py-3 mx-1 transform transition duration-200 hover:scale-105 hover:bg-orange-300 hover:shadow-diagonal">
+      <a
+        className={cn(
+          "px-3",
+          bottom ? "py-1" : "py-3",
+          "mx-1",
+          "transition",
+          "duration-200",
+          "transform",
+          { "hover:scale-105": !bottom },
+          bottom ? "hover:bg-blue-800" : "hover:bg-orange-300",
+          "hover:shadow-diagonal",
+          { "w-full": bottom },
+          { "text-left": bottom },
+          { "transform hover:translate-x-2": bottom },
+          { "mt-1": bottom }
+        )}
+      >
         {children}
       </a>
     </Link>
