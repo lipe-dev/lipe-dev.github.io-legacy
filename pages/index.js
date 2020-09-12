2690612c27514e869c86030ff369678a398f2784
Felipe Pereira <fe@lipe.dev>
2020-09-12 00:14:45 -0300

    add previews and footer to index page

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,51 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout/layout";
 import { getSortedPostsData } from "../lib/posts";
-import Link from "next/link";
-import Date from "../components/date";
+import Preview from "../components/preview/preview";
 
-export default function Home({ allPostsData }) {
+export default function Home({ techPostData, folioPostData, blogPostData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle} | Home</title>
       </Head>
-      <section>
-        <h2>Blog</h2>
-        <ul>
-          {allPostsData.map(({ id, date, title }) => (
-            <li key={id}>
-              <Link href="/posts/[id]" as={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <Preview
+        title="What's new around here"
+        posts={blogPostData}
+        morePosts="More from the blog"
+      />
+      <Preview
+        title="Tech I recently fell in love with..."
+        posts={techPostData}
+        round
+        morePosts="See all tech I've ever loved!"
+      />
+      <Preview
+        title="My latest projects"
+        posts={folioPostData}
+        morePosts="See my full folio here"
+      />
     </Layout>
   );
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  const techPostData = getSortedPostsData({
+    filters: { categories: ["tech"] },
+    limit: 4,
+  });
+  const folioPostData = getSortedPostsData({
+    filters: { categories: ["folio"] },
+    limit: 3,
+  });
+  const blogPostData = getSortedPostsData({
+    limit: 3,
+  });
   return {
     props: {
-      allPostsData,
+      techPostData,
+      folioPostData,
+      blogPostData,
     },
   };
 }
