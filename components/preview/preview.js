6ade2bede11f20595f181bd3551bf253e47ec788
Felipe Pereira <fe@lipe.dev>
2020-09-12 00:13:09 -0300

    add the preview sections actual content

--- components/preview/preview.js
+++ components/preview/preview.js
@@ -1,7 +1,23 @@
-export default function Preview({ title, category }) {
+import PostPreview from "./postPreview";
+import cn from "classnames";
+import Button from "../button";
+
+export default function Preview({ title, posts, round, morePosts }) {
   return (
-    <section>
-      <h1>{title}</h1>
+    <section className={cn("mt-16", { "bg-gray-200": round })}>
+      <div className={cn("mb-16 container mx-auto", { "pt-4 pb-8": round })}>
+        <h1 className="text-center text-5xl my-8">{title}</h1>
+
+        <div className={cn("flex", "justify-center", { "mb-8": !round })}>
+          {posts?.map((post) => (
+            <PostPreview key={post.id} round={round} post={post} />
+          ))}
+        </div>
+
+        <div className="text-center">
+          <Button primary>{morePosts}</Button>
+        </div>
+      </div>
     </section>
   );
 }
