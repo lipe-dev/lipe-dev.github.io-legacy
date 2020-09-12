94abe3eb036d2e9ea20fca68aa4ca5616542958c
Felipe Pereira <fe@lipe.dev>
2020-09-12 00:07:46 -0300

    add button component

---
+++ components/button.js
@@ -0,0 +1,38 @@
+import cn from "classnames";
+import Link from "next/link";
+
+export default function Button({ Component, children, primary, ...props }) {
+  const classNames = cn(
+    "box-border",
+    "inline-block",
+    "p-4",
+    "mx-auto",
+    "mt-4",
+    "mb-4",
+    "font-bold",
+    "text-gray-200",
+    "uppercase",
+    "transition",
+    "duration-200",
+    "transform",
+    primary ? "bg-orange-400" : "bg-blue-700",
+    "hover:shadow-diagonal",
+    "hover:scale-105"
+  );
+
+  if (Component === Link) {
+    return (
+      <Component {...props}>
+        <a className={classNames}>{children}</a>
+      </Component>
+    );
+  }
+  if (Component) {
+    return <Component {...props}>{children}</Component>;
+  }
+  return (
+    <button {...props} className={classNames}>
+      {children}
+    </button>
+  );
+}
