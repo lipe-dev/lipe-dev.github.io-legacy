447ade5f152f6b21bcbf5ff8c731f43ecc1f626a
Felipe Pereira <fe@lipe.dev>
2020-09-13 00:31:58 -0300

    change post preview to improve styling

--- components/preview/postPreview.js
+++ components/preview/postPreview.js
@@ -1,78 +1,89 @@
 import Link from "next/link";
 import cn from "classnames";
-import Date from "../post/date";
+import Date from "../date";
 import Button from "../button";
 
 export default function PostPreview({ post, round }) {
   return (
-    <div
-      className={cn(
-        round ? "w-1/4" : "w-1/3",
-        "mx-2",
-        { "bg-gray-200": !round },
-        { "hover:shadow-straight": !round },
-        "transform hover:scale-105",
-        "transition",
-        "duration-200",
-        "flex",
-        "flex-col",
-        "rounded-lg",
-        "overflow-hidden",
-        { "pb-4": round }
-      )}
-    >
+    <div className={cn(round ? "w-1/4" : "w-1/2", "my-4")}>
       <div
         className={cn(
-          "overflow-hidden",
-          { "rounded-full": round },
-          round ? "w-64 h-64" : "w-full",
-          { "mx-auto": round }
-        )}
-      >
-        <img
-          src={round ? post.image : post.banner}
-          alt={post.title}
-          className={cn(
-            "transform",
-            round ? "hover:scale-110" : "hover:scale-125",
-            round ? "object-contain" : "object-cover",
-            round ? "w-64 h-64" : "w-full",
-            "transition",
-            "duration-1000",
-            { "rounded-full": round },
-            { "mx-auto": round },
-            { "bg-white": round }
-          )}
-        />
-      </div>
-      {!round && (
-        <div className="mx-4 mt-2 italic">
-          <Date dateString={post.date} />
-        </div>
-      )}
-      <h2
-        className={cn(
-          "text-2xl",
           "mx-4",
-          "mb-4",
-          "leading-snug",
-          { "mt-4": round },
-          { "text-center": round }
+          { "bg-gray-200": !round },
+          { "hover:shadow-straight": !round },
+          "transform hover:scale-105",
+          "transition",
+          "duration-200",
+          "flex",
+          "flex-col",
+          "rounded-lg",
+          "overflow-hidden",
+          { "pb-4": round },
+          "flex-shrink"
         )}
       >
-        {post.title}
-      </h2>
-      <p
-        className={cn("leading-snug", "text-lg", "mx-4", {
-          "text-center": round,
-        })}
-      >
-        {post.excerpt}
-      </p>
-      <div className="mt-auto text-center">
-        <Button Component={Link} href={`/posts/${post.id}`}>
-          Read More
-        </Button>
+        <div
+          className={cn(
+            "overflow-hidden",
+            { "rounded-full": round },
+            round ? "w-64 h-64" : "w-full",
+            "max-w-full",
+            { "mx-auto": round },
+            "relative"
+          )}
+        >
+          <img
+            src={round ? post.image : post.banner}
+            alt={post.title}
+            className={cn(
+              "transform",
+              round ? "hover:scale-110" : "hover:scale-110",
+              round ? "object-contain" : "object-cover",
+              round ? "w-64 h-64" : "w-full",
+              "transition",
+              "duration-1000",
+              { "rounded-full": round },
+              { "mx-auto": round },
+              { "bg-white": round }
+            )}
+          />
+          {!round && (
+            <h2 className="absolute bottom-0 text-3xl p-4 text-white bg-blue-900 w-full bg-opacity-25">
+              {post.title}
+            </h2>
+          )}
+        </div>
+        {!round && (
+          <div className="mx-4 my-2 italic text-right text-lg text-gray-600">
+            <Date dateString={post.date} />
+          </div>
+        )}
+        {round && (
+          <h2
+            className={cn(
+              "text-2xl",
+              "mx-4",
+              "mb-4",
+              "leading-snug",
+              { "mt-4": round },
+              { "text-center": round }
+            )}
+          >
+            {post.title}
+          </h2>
+        )}
+        <p
+          className={cn("leading-snug", "text-xl", "mx-4", "text-gray-800", {
+            "text-center": round,
+          })}
+        >
+          {post.excerpt}
+        </p>
+        <div className="mt-auto text-center">
+          <Button Component={Link} href={`/posts/${post.id}`}>
+            Read More +
+          </Button>
+        </div>
       </div>
     </div>
   );
--- components/preview/preview.js
+++ components/preview/preview.js
@@ -1,22 +1,43 @@
+import Link from "next/link";
+
 import PostPreview from "./postPreview";
 import cn from "classnames";
 import Button from "../button";
 
-export default function Preview({ title, posts, round, morePosts }) {
+export default function Preview({
+  title,
+  home,
+  posts,
+  round,
+  morePosts,
+  linkTo,
+}) {
   return (
-    <section className={cn("mt-16", { "bg-gray-200": round })}>
-      <div className={cn("mb-16 container mx-auto", { "pt-4 pb-8": round })}>
+    <section className={cn({ "mt-16": home }, { "bg-gray-200": round })}>
+      <div
+        className={cn({ "mb-16": home }, "container", "mx-auto", {
+          "pt-4 pb-8": round,
+        })}
+      >
         <h1 className="text-center text-5xl my-8">{title}</h1>
 
-        <div className={cn("flex", "justify-center", { "mb-8": !round })}>
+        <div
+          className={cn("flex", "flex-wrap", "justify-center", {
+            "mb-8": !round,
+          })}
+        >
           {posts?.map((post) => (
             <PostPreview key={post.id} round={round} post={post} />
           ))}
         </div>
 
-        <div className="text-center">
-          <Button primary>{morePosts}</Button>
-        </div>
+        {morePosts && (
+          <div className="text-center">
+            <Button primary Component={Link} href={linkTo}>
+              {morePosts}
+            </Button>
+          </div>
+        )}
       </div>
     </section>
   );
