39cbf110f0e3e3fc5d6b1bd560a9e7f9d0f64b86
Felipe Pereira <fe@lipe.dev>
2020-09-13 00:30:05 -0300

    add non home header styling

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,38 +1,44 @@
+import cn from "classnames";
+
 import Tagline from "../tagline";
-import Link from "next/link";
 import Nav from "../nav/nav";
 import Social from "../social/social";
 import { siteName } from "./layout";
 
 export default function Header({ home }) {
   return (
-    <header className="container min-w-full bg-orange-400 shadow-straight">
-      {home ? (
-        <div className="flex items-center mx-auto justify-center container">
-          <img
-            className="rounded-full w-48 mx-10 my-8"
-            src="/images/profile.jpg"
-            alt={siteName}
-          />
-          <div className=" text-gray-800">
-            <h1 className="text-4xl font-bold my-2">{siteName}</h1>
-            <Tagline />
-          </div>
-        </div>
-      ) : (
-        <>
-          <Link href="/">
-            <a>
-              <img src="/images/profile.jpg" alt={name} />
-            </a>
-          </Link>
-          <h2>
-            <Link href="/">
-              <a>{name}</a>
-            </Link>
-          </h2>
-        </>
+    <header
+      className={cn(
+        "container",
+        "min-w-full",
+        "bg-orange-400",
+        "shadow-straight",
+        "mb-1",
+        { flex: !home },
+        { "items-center": !home },
+        { "py-2": home }
       )}
+    >
+      <div className="flex items-center mx-auto justify-center container flex-shrink-0">
+        <img
+          className={cn(
+            "rounded-full",
+            home ? "w-48" : "w-16",
+            "mx-10",
+            home ? "my-8" : "my-4"
+          )}
+          src="/images/profile.jpg"
+          alt={siteName}
+        />
+        <div className={cn("text-gray-900", !home && "flex")}>
+          <h1
+            className={cn(home ? "text-4xl" : "text-2xl", "font-bold", "my-2")}
+          >
+            {siteName}
+          </h1>
+          <Tagline home={home} />
+        </div>
+      </div>
       <Nav home={home} />
       <Social />
     </header>
