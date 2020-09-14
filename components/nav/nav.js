fb80938781c0a91dd78e81a95da490a6960fc518
Felipe Pereira <fe@lipe.dev>
2020-09-14 18:21:22 -0300

    update nav to grow when needed

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -14,7 +14,8 @@ export default function Nav({ home, bottom }) {
         "my-4",
         bottom ? "text-gray-200" : "text-gray-800",
         { "mt-8": bottom },
-        { "flex-col": bottom }
+        { "flex-col": bottom },
+        "flex-grow"
       )}
     >
       <NavItem href="/" bottom={bottom}>
@@ -29,10 +30,10 @@ export default function Nav({ home, bottom }) {
       <NavItem href="/posts/list/1" bottom={bottom}>
         Blog
       </NavItem>
-      <NavItem href="/" bottom={bottom}>
-        About
+      <NavItem href="/cv" bottom={bottom}>
+        CV
       </NavItem>
-      <NavItem href="/" bottom={bottom}>
+      <NavItem href="/chat" bottom={bottom}>
         Chat
       </NavItem>
     </nav>
